5d74c5d4cb0c52a28c50b2d6911fd9e3d3fa9950
DelJoabe Simão <simaojoabecosta@hotmail.com>
2023-01-22 17:14:53 -0300

    cria css para pagina principal

--- javascript/main.js
+++ javascript/main.js
@@ -7,6 +7,6 @@ result.innerHTML =  `<img src="./image/download.jpeg" />`
 }
 const mostraEspec = (seletor)=>{
     const especificacoes = document.querySelector(seletor)
-    especificacoes.innerHTML = `<a href="./main_celulares.html">celular motorola 8GB ram, 12megaPix,128GB espaço interno</a> `
+    especificacoes.innerHTML = `<a href="./main_celulares.html"><p>celular motorola 8GB ram, 12megaPix,128GB espaço interno </p></a> `
 
 }
