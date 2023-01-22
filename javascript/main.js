1d20fa347bac300fbcb413a95f8782d7f55c9d89
DelJoabe Simão <simaojoabecosta@hotmail.com>
2023-01-22 15:11:01 -0300

    cria link para produto

--- javascript/main.js
+++ javascript/main.js
@@ -7,6 +7,6 @@ result.innerHTML =  `<img src="./image/download.jpeg" />`
 }
 const mostraEspec = (seletor)=>{
     const especificacoes = document.querySelector(seletor)
-    especificacoes.innerHTML = "celular motorola 8GB ram, 12megaPix,128GB espaço interno"
+    especificacoes.innerHTML = `<a href="./main_celulares.html">celular motorola 8GB ram, 12megaPix,128GB espaço interno</a> `
 
 }
